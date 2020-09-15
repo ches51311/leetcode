c4573362297e225234fd0963003c366c05ee9fea
swimdi <swimdi@skymizer.com>
2020-09-15 03:17:52 +0000

    edit 75

--- 75/sort_colors.cpp
+++ 75/sort_colors.cpp
@@ -6,13 +6,16 @@ using std::vector;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int a=0, b=0;
-        for (auto i = begin(nums); i != end(nums); i++){
-            if (*i == 0) a++;
-            else if (*i == 1) b++;
-        }
+        int a = 0, b = 0;
         for (vector<int>::size_type i = 0; i < nums.size(); i++){
-            nums[i] = i < a ? 0 : i < a+b ? 1 : 2;
+            if (nums[i] == 0){
+                nums[a] = 0;
+                a++;
+            }
+            else if (nums[i] == 1) b++;
+        }
+        for (vector<int>::size_type i = a; i < nums.size(); i++){
+            nums[i] = i < (a+b) ? 1 : 2;
         }
     }
 };
