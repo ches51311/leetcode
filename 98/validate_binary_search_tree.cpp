fe9bf7fa307a362a23992ab402f7da7c5b397c13
周咏廸 <swimdi@skymizer.com>
2020-10-18 17:20:29 +0800

    edit 98

--- 98/validate_binary_search_tree.cpp
+++ 98/validate_binary_search_tree.cpp
@@ -7,17 +7,8 @@ public:
     }
     bool helper(TreeNode* root, int *lowerbound, int *upperbound) {
         if (!root) return true;
-        if (root->left) {
-            if (root->left->val >= root->val) return false;
-            if (lowerbound && root->left->val <= *lowerbound) return false;
-            if (upperbound && root->left->val >= *upperbound) return false;
-        }
-        if (root->right) {
-            if (root->right->val <= root->val) return false;
-            if (lowerbound && root->right->val <= *lowerbound) return false;
-            if (upperbound && root->right->val >= *upperbound) return false;
-        }
-
+        if (lowerbound && root->val <= *lowerbound) return false;
+        if (upperbound && root->val >= *upperbound) return false;
         if (!helper(root->left, lowerbound, &root->val)) return false;
         if (!helper(root->right, &root->val, upperbound)) return false;
 
