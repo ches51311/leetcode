d49e4290d1d5d77afcd1524e6f2c26f7bb62b35e
周咏廸 <swimdi@skymizer.com>
2020-10-18 17:16:11 +0800

    add define of link list

--- include/define.h
+++ include/define.h
@@ -2,7 +2,17 @@
 #include <string>
 #include <vector>
 
-//Definition for a binary tree node.
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+// Definition for a binary tree node.
 struct TreeNode {
     int val;
     TreeNode *left;
